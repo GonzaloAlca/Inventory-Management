e11be3b1377091697134131c8efb2554eafacdd4
Gonzalo <galca@americatv.pe>
2025-02-13 06:48:43 -0500

    html init

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+const name = document.getElementById("name");
+const cantidad = document.getElementById("cantidad");
+const precio = document.getElementById("precio");
+const categoria = document.getElementById("categoria");
+const addButton = document.getElementById("addButton");
+const tablaInventario = document.getElementById('tablaInventario');
+
+
+
